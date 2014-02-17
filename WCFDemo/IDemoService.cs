a74c56e7658bb270a106fb97a375f6c962da2d07
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2014-02-17 16:51:22 +0100

    remove replyaction attribute

--- WCFDemo/IDemoService.cs
+++ WCFDemo/IDemoService.cs
@@ -8,7 +8,7 @@ namespace WCFDemo
     {
         
         // CODEGEN: Generating message contract since the wrapper namespace (http://tempuri.org/) of message TestRequest does not match the default value (http://tempuri.org)
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org:testIn", ReplyAction="*")]
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org:testIn")]
         [System.ServiceModel.FaultContractAttribute(typeof(ErrorMessage), Action="http://tempuri.org:testIn", Name="ErrorMessage", Namespace="http://tempuri.org/")]
         [System.ServiceModel.XmlSerializerFormatAttribute(SupportFaults=true)]
         TestResponse Test(TestRequest request);
